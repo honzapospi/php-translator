859b643aadaca0ea26dec702e2fcd745030ae2e4
Jan Pospisil <me@jan-pospisil.cz>
2017-02-15 09:25:56 +0100

    Throw Error if try to translate empty message.

--- src/translator.js
+++ src/translator.js
@@ -6,12 +6,16 @@ var Translator = function (dict, lang) {
 Translator.prototype.translate = function (msg, count) {
     if(!msg)
         throw new Error('Message to translate can not be empty.');
+    if(!this.dictionary[msg])
+        return msg;
     return this.dictionary[msg][this.lang];
 }
 
 Translator.prototype.translateTo = function (msg, lang) {
     if(!msg)
         throw new Error('Message to translate can not be empty.');
+    if(!this.dictionary[msg])
+        return msg;
     return this.dictionary[msg][lang];
 }
 
