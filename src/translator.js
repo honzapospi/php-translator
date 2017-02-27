15ba9a604b0d44017d96009879b1a347904fcdb5
Jan Pospisil <me@jan-pospisil.cz>
2017-02-27 11:39:57 +0100

    Print missing translation to console

--- src/translator.js
+++ src/translator.js
@@ -4,18 +4,18 @@ var Translator = function (dict, lang) {
 }
 
 Translator.prototype.translate = function (msg, count) {
-    if(!msg)
-        throw new Error('Message to translate can not be empty.');
-    if(!this.dictionary[msg])
-        return msg;
-    return this.dictionary[msg][this.lang];
+    return this.translateTo(msg, this.lang);
 }
 
 Translator.prototype.translateTo = function (msg, lang) {
     if(!msg)
         throw new Error('Message to translate can not be empty.');
-    if(!this.dictionary[msg])
+    if(msg.substr(0, 3) != 'tr_')
         return msg;
+    if(!this.dictionary[msg]){
+        console.log('Missing translation for '+msg+' in '+lang);
+        return msg;
+    }
     return this.dictionary[msg][lang];
 }
 
